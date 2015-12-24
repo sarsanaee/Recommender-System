0287555fa6813738d479c01cb3c2d11e253ccf47
alireza <sarsanaee@gmail.com>
2015-12-25 03:11:08 +0330

    cleanning code

--- Slope.py
+++ Slope.py
@@ -44,69 +44,58 @@ def find_difference(result,data, user):
             error_list.append(abs(data[user][i] - result[i]))
         except:
             max_error = max_error
-    #print "fuck"
-    #print (max_error)
     return max_error, error_list
 
 
 if __name__ == '__main__':
-    '''
-    userdata = dict(
-        alice=dict(squid=1.0,
-                   cuttlefish=0.5,
-                   octopus=0.2),
-        bob=dict(squid=1.0,
-                 octopus=0.5,
-                 nautilus=0.2),
-        carole=dict(squid=0.2,
-                    octopus=1.0,
-                    cuttlefish=0.4,
-                    nautilus=0.4),
-        dave=dict(cuttlefish=0.9,
-                  octopus=0.4,
-                  nautilus=0.5),
-        )
-    '''
 
     s = SlopeOne()
-    errors = []
 
-    data = {}
-    with open('/home/alireza/Desktop/ml100k/u1.base', 'r') as fp:
-        for line in fp:
-            temp = line.split()
-            if(data.has_key(temp[0])):
-                data[temp[0]][temp[1]] = int(temp[2])
-            else:
-                dictionary = {}
-                dictionary[temp[1]] = int(temp[2])    
-                data[temp[0]] = dictionary
-            
-    
-    s.update(data)
-    
-    
-    
-    ## 
+    for i in range(1,6):
+
+        s.diffs = {}
+        s.freqs = {}
+
+        errors = []
+
+        data = {}
+        with open('/home/alireza/coding/courses/AI/Recommender-System/dataset/ml-100k/u' + str(i) + '.base', 'r') as fp:
+            for line in fp:
+                temp = line.split()
+                if(data.has_key(temp[0])):
+                    data[temp[0]][temp[1]] = int(temp[2])
+                else:
+                    dictionary = {}
+                    dictionary[temp[1]] = int(temp[2])
+                    data[temp[0]] = dictionary
+
+
+        s.update(data)
+
+
+
+    ##
     ###  Prediction :)))))
-    
 
-    pre_user = '1'
-    dictionary = {}
-    with open('/home/alireza/Desktop/ml100k/u1.test', 'r') as fp:
-        for line in fp:
-            temp = line.split()
-            if(pre_user != temp[0]):
-                errors.append(find_difference(s.predict(dictionary), data, pre_user))
-                dictionary = {}
-                dictionary[temp[1]] = int(temp[2])
-                
-            else:
-                dictionary[temp[1]] = int(temp[2])
-            
-            pre_user = temp[0]
-            
-    total_errors = 0
-    for i in errors:
-        total_errors = total_errors + i[0]/len(i[1])
-    print total_errors/len(errors)
+
+        pre_user = '1'
+        dictionary = {}
+
+        with open('/home/alireza/coding/courses/AI/Recommender-System/dataset/ml-100k/u' + str(i) + '.test', 'r') as fp:
+            for line in fp:
+                temp = line.split()
+                if(pre_user != temp[0]):
+                    errors.append(find_difference(s.predict(dictionary), data, pre_user))
+                    dictionary = {}
+                    dictionary[temp[1]] = int(temp[2])
+
+                else:
+                    dictionary[temp[1]] = int(temp[2])
+
+                pre_user = temp[0]
+
+        total_errors = 0
+        for i in errors:
+            total_errors = total_errors + i[0]/len(i[1])
+        print total_errors/len(errors)
+
